fbc7f4c27ee97a73578bff13c0c2520e175925de
Adam Murray <adamjmurray@gmail.com>
2012-08-15 19:33:16 -0700

    Progress on Live POC. A receiver for the step sequencer drives a MIDI instrument.

--- launchpad-sequencer-js/launchpad-sequencer.js
+++ launchpad-sequencer-js/launchpad-sequencer.js
@@ -1,6 +1,6 @@
 outlets = 3;
 
-function include(n){var f=new File(n),t=[],e=f.eof,i=0;if(f.isopen){for(;i<e;i++)t+=f.readchars(1);f.close();eval(t+'');}else error("Missing required file: "+n);}
+function include(n){var f=new File(n),t=[],e=f.eof,i=0;if(f.isopen){for(;i<e;i++)t+=f.readchars(1);f.close();eval(t+'');}else error("Missing required file: "+n+"\n");}
 include('class.js');
 include('launchpad.js');
 include('sequencer.js');
